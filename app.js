339148c3573ebef854455efc744bc6c731a6f339
Pearyman <liyananhappy@sina.cn>
2017-05-01 18:16:53 +0800

    update miniapp

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 //app.js
+const AV = require('./libs/av-weapp-min.js')
 App({
-  onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-  },
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              console.log(666)
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
-      })
-    }
-  },
-  globalData:{
-    userInfo:null
-  }
+	onLaunch: function() {
+		//调用API从本地缓存中获取数据
+		var logs = wx.getStorageSync('logs') || []
+		logs.unshift(Date.now())
+		wx.setStorageSync('logs', logs)
+		// bind leancloud
+		AV.init({
+			appId: '1KQ32oO43Y8Bk8f3smsO8drp-gzGzoHsz',
+			appKey: 'sREtK6AxYhOAgQ3jFinuxaX2',
+		});
+	},
+	getUserInfo: function(cb) {
+		var that = this
+		if (this.globalData.userInfo) {
+			typeof cb == "function" && cb(this.globalData.userInfo)
+		} else {
+			//调用登录接口
+			wx.login({
+				success: function() {
+					wx.getUserInfo({
+						success: function(res) {
+							that.globalData.userInfo = res.userInfo
+							// console.log(666)
+							typeof cb == "function" && cb(that.globalData.userInfo)
+						}
+					})
+				}
+			})
+		}
+	},
+	globalData: {
+		userInfo: null
+	}
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,41 @@
 //index.js
-//获取应用实例
+const AV = require('../../libs/av-weapp-min.js')
 var app = getApp()
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
+	data: {
+		motto: 'Hello World',
+		userInfo: {}
+	},
+	//事件处理函数
+	bindViewTap: function() {
+		wx.navigateTo({
+			url: '../logs/logs'
+		})
+	},
+	onLoad: function() {
+		console.log('onLoad')
+		this.saveUser()
+
+	},
+	saveUser() {
+		var that = this
+		//调用应用实例的方法获取全局数据
+		app.getUserInfo(function(userInfo) {
+			//update data
+			that.setData({
+				userInfo: userInfo
+			})
+			let nickName = that.data.userInfo.nickName
+			let avatarUrl = that.data.userInfo.avatarUrl
+			let gender = that.data.userInfo.gender
+			AV.Query.doCloudQuery('insert into _User(username, password , avatarUrl, gender) values("' + nickName + '","123", "' + avatarUrl + '",' + gender + ')').then(function(data) {
+				// data 中的 results 是本次查询返回的结果，AV.Object 实例列表
+				var results = data.results;
+			}, function(error) {
+				//查询失败，查看 error
+				console.log(error);
+			});
+
+		})
+	}
 })
