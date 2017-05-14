705af359bfe00423ae741d1923b7432824dbbf03
Pearyman <liyananhappy@sina.cn>
2017-05-14 21:05:49 +0800

    update

--- pages/form/index.js
+++ pages/form/index.js
@@ -9,7 +9,15 @@ Page({
 	},
 	onLoad: function() {
 		console.log('onLoad')
-		// this.saveUser()
+		// this.getUser()
+	},
+	getUser() {
+		wx.getStorage({
+		  key: 'userInfo',
+		  success: function(res) {
+		      console.log(res.data)
+		  }
+		})
 	},
 	uploadPic() {
 		var that = this
@@ -34,5 +42,25 @@ Page({
 				).catch(console.error);
 			}
 		})
+	},
+	save() {
+	// 	wx.getStorage({
+	// 	  key: 'userInfo',
+	// 	  success: function(res) {
+	// 				let nickName = res.data.nickName
+	// 				let avatarUrl = res.data.avatarUrl
+	// 	  }
+	// 	})
+	// 	let nickName = that.data.userInfo.nickName
+	// 	let avatarUrl = that.data.userInfo.avatarUrl
+	// 	let gender = that.data.userInfo.gender
+	// 	AV.Query.doCloudQuery('insert into _User(username, password , avatarUrl, gender) values("' + nickName + '","123", "' + avatarUrl + '",' + gender + ')').then(function(data) {
+	// 		// data 中的 results 是本次查询返回的结果，AV.Object 实例列表
+	// 		var results = data.results;
+	// 	}, function(error) {
+	// 		//查询失败，查看 error
+	// 		console.log(error);
+	// 	});
 	}
+
 })
